0eed39b7469062168c3ec3427a0af1ea85efba61
Johnny Proano <djimageonline@gmail.com>
2022-12-27 01:06:26 -0500

    added styling to the cards, shadow, character limit for short bio. updated button size and card size with 6 total placeholder cards

--- src/TwitterFeed.jsx
+++ src/TwitterFeed.jsx
@@ -10,27 +10,91 @@ export function TwitterFeed() {
             className="profile-photo"
             src="https://pbs.twimg.com/profile_images/3478244961/01ebfc40ecc194a2abc81e82ab877af4_400x400.jpeg"
           />
-          <h2>Dwayne The Rock</h2>
+          <h3>Dwayne The Rock</h3>
           <p>
             Arrr me hearties! Make traditional Latin walk the plank and opt for pirate lorem ipsum for your next high
-            seas design adventure.
+            seas design adventure. Arrr me hearties! Make traditional Latin walk the plank and opt for pirate lorem
+            ipsum for your next high seas design adventure. Arrr me hearties! Make traditional Latin walk the plank and
+            opt for pirate lorem ipsum for your next high seas design adventure. Arrr me hearties! Make traditional
+            Latin walk the plank and opt for pirate lorem ipsum for your next high seas design adventure.
           </p>
-          <a className="Twitter-profile" href="https://twitter.com/TheRock" />
-          <a
-            className="twitter-timeline"
-            href="https://twitter.com/TheRock?ref_src=twsrc%5Etfw"
-            tweet-limit="Range: 1-5"
-            data-height="200"
-            data-width="300"
-            data-chrome="nofooter"
-            data-border-color="#a80000"
-          >
-            Tweets by TheRock
-          </a>{" "}
           <div className="but-container">
             <button>Twitter</button>
             <button>LinkedIn</button>
             <button>Email</button>
+          </div>
+          <div className="twitter-container">
+            <a
+              className="twitter-timeline"
+              href="https://twitter.com/TheRock?ref_src=twsrc%5Etfw"
+              tweet-limit="Range: 1-5"
+              data-height="200"
+              data-width="300"
+              data-chrome="nofooter"
+              data-border-color="#a80000"
+            ></a>{" "}
+          </div>
+        </div>
+
+        <div className="card">
+          <img
+            className="profile-photo"
+            src="https://pbs.twimg.com/profile_images/1604568346787299329/Y0kJbT20_400x400.jpg"
+          />
+          <h3>Leo Messi</h3>
+          <p>
+            Arrr me hearties! Make traditional Latin walk the plank and opt for pirate lorem ipsum for your next high
+            seas design adventure. Arrr me hearties! Make traditional Latin walk the plank and opt for pirate lorem
+            ipsum for your next high seas design adventure. Arrr me hearties! Make traditional Latin walk the plank and
+            opt for pirate lorem ipsum for your next high seas design adventure. Arrr me hearties! Make traditional
+            Latin walk the plank and opt for pirate lorem ipsum for your next high seas design adventure.
+          </p>
+          <div className="but-container">
+            <button>Twitter</button>
+            <button>LinkedIn</button>
+            <button>Email</button>
+          </div>
+          <div className="twitter-container">
+            <a
+              className="twitter-timeline"
+              href="https://twitter.com/WeAreMessi?ref_src=twsrc%5Etfw"
+              tweet-limit="Range: 1-5"
+              data-height="200"
+              data-width="300"
+              data-chrome="nofooter"
+              data-border-color="#a80000"
+            ></a>{" "}
+          </div>
+        </div>
+
+        <div className="card">
+          <img
+            className="profile-photo"
+            src="https://pbs.twimg.com/profile_images/725013638411489280/4wx8EcIA_400x400.jpg"
+          />
+          <h3>Homer Simpson</h3>
+          <p>
+            Arrr me hearties! Make traditional Latin walk the plank and opt for pirate lorem ipsum for your next high
+            seas design adventure. Arrr me hearties! Make traditional Latin walk the plank and opt for pirate lorem
+            ipsum for your next high seas design adventure. Arrr me hearties! Make traditional Latin walk the plank and
+            opt for pirate lorem ipsum for your next high seas design adventure. Arrr me hearties! Make traditional
+            Latin walk the plank and opt for pirate lorem ipsum for your next high seas design adventure.
+          </p>
+          <div className="but-container">
+            <button>Twitter</button>
+            <button>LinkedIn</button>
+            <button>Email</button>
+          </div>
+          <div className="twitter-container">
+            <a
+              className="twitter-timeline"
+              href="https://twitter.com/HomerJSimpson?ref_src=twsrc%5Etfw"
+              tweet-limit="Range: 1-5"
+              data-height="200"
+              data-width="300"
+              data-chrome="nofooter"
+              data-border-color="#a80000"
+            ></a>{" "}
           </div>
         </div>
 
@@ -39,56 +103,91 @@ export function TwitterFeed() {
             className="profile-photo"
             src="https://pbs.twimg.com/profile_images/3478244961/01ebfc40ecc194a2abc81e82ab877af4_400x400.jpeg"
           />
-          <h2>Dwayne The Rock</h2>
+          <h3>Dwayne The Rock</h3>
           <p>
             Arrr me hearties! Make traditional Latin walk the plank and opt for pirate lorem ipsum for your next high
-            seas design adventure.
+            seas design adventure. Arrr me hearties! Make traditional Latin walk the plank and opt for pirate lorem
+            ipsum for your next high seas design adventure. Arrr me hearties! Make traditional Latin walk the plank and
+            opt for pirate lorem ipsum for your next high seas design adventure. Arrr me hearties! Make traditional
+            Latin walk the plank and opt for pirate lorem ipsum for your next high seas design adventure.
           </p>
-          <a className="Twitter-profile" href="https://twitter.com/TheRock" />
-          <a
-            className="twitter-timeline"
-            href="https://twitter.com/TheRock?ref_src=twsrc%5Etfw"
-            tweet-limit="Range: 1-5"
-            data-height="200"
-            data-width="300"
-            data-chrome="nofooter"
-            data-border-color="#a80000"
-          >
-            Tweets by TheRock
-          </a>{" "}
           <div className="but-container">
             <button>Twitter</button>
             <button>LinkedIn</button>
             <button>Email</button>
+          </div>
+          <div className="twitter-container">
+            <a
+              className="twitter-timeline"
+              href="https://twitter.com/TheRock?ref_src=twsrc%5Etfw"
+              tweet-limit="Range: 1-5"
+              data-height="200"
+              data-width="300"
+              data-chrome="nofooter"
+              data-border-color="#a80000"
+            ></a>{" "}
           </div>
         </div>
 
         <div className="card">
           <img
             className="profile-photo"
-            src="https://pbs.twimg.com/profile_images/3478244961/01ebfc40ecc194a2abc81e82ab877af4_400x400.jpeg"
+            src="https://pbs.twimg.com/profile_images/1604568346787299329/Y0kJbT20_400x400.jpg"
           />
-          <h2>Dwayne The Rock</h2>
+          <h3>Leo Messi</h3>
           <p>
             Arrr me hearties! Make traditional Latin walk the plank and opt for pirate lorem ipsum for your next high
-            seas design adventure.
+            seas design adventure. Arrr me hearties! Make traditional Latin walk the plank and opt for pirate lorem
+            ipsum for your next high seas design adventure. Arrr me hearties! Make traditional Latin walk the plank and
+            opt for pirate lorem ipsum for your next high seas design adventure. Arrr me hearties! Make traditional
+            Latin walk the plank and opt for pirate lorem ipsum for your next high seas design adventure.
           </p>
-          <a className="Twitter-profile" href="https://twitter.com/TheRock" />
-          <a
-            className="twitter-timeline"
-            href="https://twitter.com/TheRock?ref_src=twsrc%5Etfw"
-            tweet-limit="Range: 1-5"
-            data-height="200"
-            data-width="300"
-            data-chrome="nofooter"
-            data-border-color="#a80000"
-          >
-            Tweets by TheRock
-          </a>{" "}
           <div className="but-container">
             <button>Twitter</button>
             <button>LinkedIn</button>
             <button>Email</button>
+          </div>
+          <div className="twitter-container">
+            <a
+              className="twitter-timeline"
+              href="https://twitter.com/WeAreMessi?ref_src=twsrc%5Etfw"
+              tweet-limit="Range: 1-5"
+              data-height="200"
+              data-width="300"
+              data-chrome="nofooter"
+              data-border-color="#a80000"
+            ></a>{" "}
+          </div>
+        </div>
+
+        <div className="card">
+          <img
+            className="profile-photo"
+            src="https://pbs.twimg.com/profile_images/725013638411489280/4wx8EcIA_400x400.jpg"
+          />
+          <h3>Homer Simpson</h3>
+          <p>
+            Arrr me hearties! Make traditional Latin walk the plank and opt for pirate lorem ipsum for your next high
+            seas design adventure. Arrr me hearties! Make traditional Latin walk the plank and opt for pirate lorem
+            ipsum for your next high seas design adventure. Arrr me hearties! Make traditional Latin walk the plank and
+            opt for pirate lorem ipsum for your next high seas design adventure. Arrr me hearties! Make traditional
+            Latin walk the plank and opt for pirate lorem ipsum for your next high seas design adventure.
+          </p>
+          <div className="but-container">
+            <button>Twitter</button>
+            <button>LinkedIn</button>
+            <button>Email</button>
+          </div>
+          <div className="twitter-container">
+            <a
+              className="twitter-timeline"
+              href="https://twitter.com/HomerJSimpson?ref_src=twsrc%5Etfw"
+              tweet-limit="Range: 1-5"
+              data-height="200"
+              data-width="300"
+              data-chrome="nofooter"
+              data-border-color="#a80000"
+            ></a>{" "}
           </div>
         </div>
       </div>
