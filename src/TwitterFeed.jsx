0a53420f9d13f8a0003475d1bb214a58919e1734
Johnny Proano <djimageonline@gmail.com>
2022-12-26 23:23:55 -0500

    added styling to cards detail

--- src/TwitterFeed.jsx
+++ src/TwitterFeed.jsx
@@ -6,22 +6,16 @@ export function TwitterFeed() {
       <h2>Twitter Feed</h2>
       <div className="container">
         <div className="card">
-          <div>
-            <img
-              className="profile-photo"
-              src="https://pbs.twimg.com/profile_images/3478244961/01ebfc40ecc194a2abc81e82ab877af4_400x400.jpeg"
-            />
-            <h2>Dwayne The Rock</h2>
-            <p>email</p>
-            <p>
-              Arrr me hearties! Make traditional Latin walk the plank and opt for pirate lorem ipsum for your next high
-              seas design adventure.
-            </p>
-            <button>Twitter</button>
-            <button>LinkedIn</button>
-
-            <a className="Twitter-profile" href="https://twitter.com/TheRock" />
-          </div>
+          <img
+            className="profile-photo"
+            src="https://pbs.twimg.com/profile_images/3478244961/01ebfc40ecc194a2abc81e82ab877af4_400x400.jpeg"
+          />
+          <h2>Dwayne The Rock</h2>
+          <p>
+            Arrr me hearties! Make traditional Latin walk the plank and opt for pirate lorem ipsum for your next high
+            seas design adventure.
+          </p>
+          <a className="Twitter-profile" href="https://twitter.com/TheRock" />
           <a
             className="twitter-timeline"
             href="https://twitter.com/TheRock?ref_src=twsrc%5Etfw"
@@ -33,6 +27,69 @@ export function TwitterFeed() {
           >
             Tweets by TheRock
           </a>{" "}
+          <div className="but-container">
+            <button>Twitter</button>
+            <button>LinkedIn</button>
+            <button>Email</button>
+          </div>
+        </div>
+
+        <div className="card">
+          <img
+            className="profile-photo"
+            src="https://pbs.twimg.com/profile_images/3478244961/01ebfc40ecc194a2abc81e82ab877af4_400x400.jpeg"
+          />
+          <h2>Dwayne The Rock</h2>
+          <p>
+            Arrr me hearties! Make traditional Latin walk the plank and opt for pirate lorem ipsum for your next high
+            seas design adventure.
+          </p>
+          <a className="Twitter-profile" href="https://twitter.com/TheRock" />
+          <a
+            className="twitter-timeline"
+            href="https://twitter.com/TheRock?ref_src=twsrc%5Etfw"
+            tweet-limit="Range: 1-5"
+            data-height="200"
+            data-width="300"
+            data-chrome="nofooter"
+            data-border-color="#a80000"
+          >
+            Tweets by TheRock
+          </a>{" "}
+          <div className="but-container">
+            <button>Twitter</button>
+            <button>LinkedIn</button>
+            <button>Email</button>
+          </div>
+        </div>
+
+        <div className="card">
+          <img
+            className="profile-photo"
+            src="https://pbs.twimg.com/profile_images/3478244961/01ebfc40ecc194a2abc81e82ab877af4_400x400.jpeg"
+          />
+          <h2>Dwayne The Rock</h2>
+          <p>
+            Arrr me hearties! Make traditional Latin walk the plank and opt for pirate lorem ipsum for your next high
+            seas design adventure.
+          </p>
+          <a className="Twitter-profile" href="https://twitter.com/TheRock" />
+          <a
+            className="twitter-timeline"
+            href="https://twitter.com/TheRock?ref_src=twsrc%5Etfw"
+            tweet-limit="Range: 1-5"
+            data-height="200"
+            data-width="300"
+            data-chrome="nofooter"
+            data-border-color="#a80000"
+          >
+            Tweets by TheRock
+          </a>{" "}
+          <div className="but-container">
+            <button>Twitter</button>
+            <button>LinkedIn</button>
+            <button>Email</button>
+          </div>
         </div>
       </div>
     </div>
